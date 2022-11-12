a51b7e31a031fd79fad4a4c45b79e7e5c060912d
RobH95 <117030647+RobH95@users.noreply.github.com>
2022-11-12 19:45:08 +0000

    added array and for loops to HTML

---
+++ assets/script.js
@@ -0,0 +1,42 @@
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript']; 
+
+/*
+topics = {
+    [0] = 'HTML',
+    [1] = 'CSS',
+    [2] = 'Git'
+    ...
+}
+topics[0] == 'HTML'
+console.log(topics[0]) == 'HTML'
+
+*/
+
+for (var x = 0; x < topics.length; x++) {
+
+    console.log(topics[x]);
+}
+
+/*
+for (
+    var x = 0; 
+    x < topics.length
+    x++
+    )
+
+UPDATE example SET
+
+*/
+/*
+if (topic === 'HTML') {
+ console.log("Let's study HTML!");
+} else if (topic === 'CSS') {
+ console.log("Let's study CSS!");
+} else if (topic === 'Git') {
+ console.log("Let's study Git!");
+} else if (topic === 'JavaScript') {
+ console.log("Let's study JavaScript!");
+} else {
+ console.log('Please try again!');
+}
+*/
